b431d3471de26fa8e660fdb57b04a27971553db8
raahul131 <rryadav2025@gmail.com>
2025-07-28 14:32:43 +0545

    06: dashboard sidebar

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -206,7 +206,7 @@ export const SignInView = () => {
 
           <div
             className={
-              "bg-radial from-green-700 to-green-800 relative hidden md:flex flex-col gap-y-4 items-center justify-center"
+              "bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center"
             }
           >
             <Image
